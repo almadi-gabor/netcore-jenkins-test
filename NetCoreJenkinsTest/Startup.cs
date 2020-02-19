9cf132dff1e7a743f880388a465f5942ffb5e630
almadi-gabor <60645359+almadi-gabor@users.noreply.github.com>
2020-02-19 11:10:56 +0100

    Update Startup.cs

--- NetCoreJenkinsTest/Startup.cs
+++ NetCoreJenkinsTest/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace NetCoreJenkinsTest
 {
+    // this is the startup
     public class Startup
     {
         public Startup(IConfiguration configuration)
